d0020a0be3aa502f368c285c177832570fb293c4
Mike Reid <mreid@outsideiq.com>
2013-11-14 10:21:28 -0500

    some whitespace cleanup; add exec-notify and '@' alias

--- bash.d/ps.sh
+++ bash.d/ps.sh
@@ -3,12 +3,12 @@ alias lspse='ps auxef|less -S'
 
 function lsenv() {
 
-        if [ -z "$1" ]
-        then
-                echo "Usage: lsenv <PID>"
-                return
-        fi
-
+	if [ -z "$1" ]
+	then
+		echo "Usage: lsenv <PID>"
+		return
+	fi
+	
 	if [ ! -d "/proc/$1" ]
 	then
 		echo "PID $1 does not exist at /proc/$1"
@@ -27,14 +27,25 @@ function lsargv() {
 		return -1
 	fi
 
-        if [ ! -d "/proc/$1" ]
-        then
-                echo "PID $1 does not exist at /proc/$1"
-                return -1
-        fi
-
-        cat "/proc/$1/cmdline" | tr '\0' ' ' | LESS="" less
-
+	if [ ! -d "/proc/$1" ]
+	then
+		echo "PID $1 does not exist at /proc/$1"
+		return -1
+	fi
+	
+	cat "/proc/$1/cmdline" | tr '\0' ' ' | LESS="" less
 
 }
 
+alias @='exec-notify'
+function exec-notify() {
+
+	"$@"
+
+	local status="$?"
+	[ "0" = "$status" ] \
+		&& notify-send -h "int:transient:1" "SUCCESS" "$*" \
+		|| notify-send -h "int:transient:1" "FAILED" "$*"
+	return "$status"
+
+}
